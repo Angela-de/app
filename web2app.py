ac52ab0e8bc2bab7d688248c3b94215e6e412b60
Angela-de <you@example.com>
2019-07-18 14:49:28 +0000

    app

--- web2app.py
+++ web2app.py
@@ -28,7 +28,7 @@ def get_todos():
     return resp
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT' 5000)
+    port = os.environ.get('PORT', 5000)
     app.run(debug=True, host='0.0.0.0')
 
     
